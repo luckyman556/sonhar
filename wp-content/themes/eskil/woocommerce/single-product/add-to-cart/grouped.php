3474f586b056eb82c6d21b7a70558c50c679a830
luckyman556 <luckyman556@gmail.com>
2025-05-12 20:47:58 +0200

    some changes

--- wp-content/themes/eskil/woocommerce/single-product/add-to-cart/grouped.php
+++ wp-content/themes/eskil/woocommerce/single-product/add-to-cart/grouped.php
@@ -171,76 +171,7 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 </div>
 
 <script>
-	const buttonContainer = document.querySelector(".button-container");
 
-	document.addEventListener("DOMContentLoaded", function() {
-		const modal = document.querySelector(".modal");
-		const modalImage = document.querySelector(".modal-img");
-		const closeOverlay = document.querySelector(".overlay");
-		const prevBtn = document.querySelector(".prev");
-		const nextBtn = document.querySelector(".next");
-
-		const fabricBlocks = document.querySelectorAll(".fabric");
-
-		let currentIndex = 0;
-
-		// Показати модалку з картинкою по індексу
-		function openModal(index) {
-			const img = fabricBlocks[index].querySelector("img");
-			modalImage.src = img.src;
-			modal.classList.remove("hidden");
-
-			// Додати клас .fabric-chosen тільки активному
-			fabricBlocks.forEach((block) => block.classList.remove("fabric-chosen"));
-			fabricBlocks[index].classList.add("fabric-chosen");
-
-			currentIndex = index;
-		}
-
-		// Закрити модалку
-		function closeModal() {
-			modal.classList.add("hidden");
-		}
-
-		// Обробка кліку на блоки
-		fabricBlocks.forEach((fabric, index) => {
-			fabric.addEventListener("click", function() {
-				openModal(index);
-			});
-		});
-
-		// Стрілка "назад"
-		prevBtn.addEventListener("click", function(e) {
-			e.stopPropagation(); // Щоб клік не закривав модалку
-			currentIndex = (currentIndex - 1 + fabricBlocks.length) % fabricBlocks.length;
-			openModal(currentIndex);
-		});
-
-		// Стрілка "вперед"
-		nextBtn.addEventListener("click", function(e) {
-			e.stopPropagation();
-			currentIndex = (currentIndex + 1) % fabricBlocks.length;
-			openModal(currentIndex);
-		});
-
-		// Закриття при кліку на overlay
-		closeOverlay.addEventListener("click", function() {
-			closeModal();
-		});
-	});
-
-
-
-	buttonContainer.addEventListener("click", (e) => {
-		const clickedBtn = e.target.closest(".step-one-btn");
-
-		if (!clickedBtn) return;
-
-		const allButtons = buttonContainer.querySelectorAll(".step-one-btn");
-		allButtons.forEach(btn => btn.classList.remove("pressed-btn"));
-
-		clickedBtn.classList.add('pressed-btn');
-	});
 </script>
 <!-- Тут кінець твого коду -->
 <form class="cart grouped_form" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
